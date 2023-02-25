9d708ba8fdc0a63c8098b79abd003a2bc242ddf3
wrech86 <83070110+wrech86@users.noreply.github.com>
2023-02-25 15:04:06 +0800

    Add files via upload

---
+++ top/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="root">
+ <router-view></router-view>
+ <tabbarVue></tabbarVue>
+</div>
+ 
+</template>
+
+<script setup>
+import tabbarVue from './tabbar.vue';
+</script>
+
+<style  scoped>
+.root{
+  font-family: PingFang;
+}
+</style>
